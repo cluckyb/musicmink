fb8064458c1015c30df3e60dc36e33a6d23d92a9
Drew Besse <dtbesse@gmail.com>
2014-07-12 11:44:38 -0700

    Updating so I can work straight from the repro
    Update .gitignore to ignore folders. Also can't figure out how to have a
    file checked in and edit it but never check the update in (to keep api
    keys private) so just made a new last.fm account for now

--- MusicMinkAppLayer/Helpers/ApiKeys.cs
+++ MusicMinkAppLayer/Helpers/ApiKeys.cs
@@ -2,7 +2,7 @@
 {
     public class ApiKeys
     {
-        public const string LASTFM_API_KEY = " ";
-        public const string LASTFM_API_SECRET = " ";
+        public const string LASTFM_API_KEY = "9c4744d78821dc3c378071a421003df4";
+        public const string LASTFM_API_SECRET = "f7096bccfa42a6a4c06547e186f89ede";
     }
 }
