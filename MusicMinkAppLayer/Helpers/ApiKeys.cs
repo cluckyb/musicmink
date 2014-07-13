538f038b1af6410ab58cbc14582ca44858f576e9
Drew Besse <dtbesse@gmail.com>
2014-07-12 23:56:07 -0700

    API Key Fix
    Not sure where that secret came from but it was wrong.

--- MusicMinkAppLayer/Helpers/ApiKeys.cs
+++ MusicMinkAppLayer/Helpers/ApiKeys.cs
@@ -3,6 +3,6 @@
     public class ApiKeys
     {
         public const string LASTFM_API_KEY = "9c4744d78821dc3c378071a421003df4";
-        public const string LASTFM_API_SECRET = "f7096bccfa42a6a4c06547e186f89ede";
+        public const string LASTFM_API_SECRET = "86fb6ee363c14c09e6452dcd4aa6e02c";
     }
 }
